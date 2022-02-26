c88412c13a7d80551a9c65d4aa37532818500c6d
Sharath Chandra S <sharath.suroj@gmail.com>
2022-02-26 22:09:48 +0530

    Update main.py

--- main.py
+++ main.py
@@ -31,17 +31,21 @@ def predict():
             Fuel_Type_Diesel=1
         Year=2020-Year
         Seller_Type_Individual=request.form['Seller_Type_Individual']
+        
         if(Seller_Type_Individual=='Individual'):
             Seller_Type_Individual=1
         else:
             Seller_Type_Individual=0	
         Transmission_Mannual=request.form['Transmission_Mannual']
+        
         if(Transmission_Mannual=='Mannual'):
             Transmission_Mannual=1
         else:
             Transmission_Mannual=0
+            
         prediction=model.predict([[Present_Price,Kms_Driven2,Owner,Year,Fuel_Type_Diesel,Fuel_Type_Petrol,Seller_Type_Individual,Transmission_Mannual]])
         output=round(prediction[0],2)
+        
         if output<0:
             return render_template('index.html',prediction_texts="Sorry you cannot sell this car")
         else:
